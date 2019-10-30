cc0aab801e25cb19ce4a1e5828c3a8e3893ca6c3
Siva Sankar Raju Medisetty <SMedisetty@INHYDLSMEDISETT>
2019-10-30 12:45:52 +0530

    ++ files added

---
+++ CsharpDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CsharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Welcome ${name}");
+            Console.ReadLine();
+        }
+    }
+}
